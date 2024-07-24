38a781ceb2f0c1692fa1f33cdf91a84acd31d1e3
Yug Jadvani <yugjadvani.dev@gmail.com>
2024-07-24 23:13:54 +0530

    ass react import

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,7 @@
 'use client'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
+import React from 'react';
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
