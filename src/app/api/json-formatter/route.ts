27d889b4559c37c5cdebf4457c160fd18082b80e
yug jadvani <110123287+yugjadvani-dev@users.noreply.github.com>
2024-09-27 23:36:56 +0530

    Update route.ts

--- src/app/api/json-formatter/route.ts
+++ src/app/api/json-formatter/route.ts
@@ -1,34 +1,32 @@
 // import JSONFormatter from "json-formatter-js";
-// import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-// export async function POST(req: NextRequest) {
-//   if (req.method === "POST") {
-//     try {
-//       const { json } = await req.json();
-//       if (!json) {
-//         return NextResponse.json({
-//           status: 400,
-//           error: "Code is required",
-//         });
-//       }
-
-//       const formatter = new JSONFormatter(json, 2);
-//       const formattedJSON = formatter.render().innerHTML;
-
-//       return NextResponse.json({
-//         status: 200,
-//         formattedJSON,
-//       });
-//     } catch (error) {
-//       return NextResponse.json({
-//         status: 500,
-//         error: "Invalid JSON data",
-//       });
-//     }
-//   } else {
-//     return NextResponse.json({
-//       status: 405,
-//       message: `Method ${req.method} Not Allowed`,
-//     });
-//   }
-// }
+export async function POST(req: NextRequest) {
+  //   if (req.method === "POST") {
+  //     try {
+  //       const { json } = await req.json();
+  //       if (!json) {
+  //         return NextResponse.json({
+  //           status: 400,
+  //           error: "Code is required",
+  //         });
+  //       }
+  //       const formatter = new JSONFormatter(json, 2);
+  //       const formattedJSON = formatter.render().innerHTML;
+  //       return NextResponse.json({
+  //         status: 200,
+  //         formattedJSON,
+  //       });
+  //     } catch (error) {
+  //       return NextResponse.json({
+  //         status: 500,
+  //         error: "Invalid JSON data",
+  //       });
+  //     }
+  //   } else {
+  //     return NextResponse.json({
+  //       status: 405,
+  //       message: `Method ${req.method} Not Allowed`,
+  //     });
+  //   }
+}
