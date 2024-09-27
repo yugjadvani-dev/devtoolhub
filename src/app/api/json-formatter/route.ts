69af4e17111c2807f9ea048e1b42a54d783d6dba
yug jadvani <110123287+yugjadvani-dev@users.noreply.github.com>
2024-09-27 23:35:40 +0530

    Update route.ts

--- src/app/api/json-formatter/route.ts
+++ src/app/api/json-formatter/route.ts
@@ -1,34 +1,34 @@
-import JSONFormatter from "json-formatter-js";
-import { NextRequest, NextResponse } from "next/server";
+// import JSONFormatter from "json-formatter-js";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-  if (req.method === "POST") {
-    try {
-      const { json } = await req.json();
-      if (!json) {
-        return NextResponse.json({
-          status: 400,
-          error: "Code is required",
-        });
-      }
+// export async function POST(req: NextRequest) {
+//   if (req.method === "POST") {
+//     try {
+//       const { json } = await req.json();
+//       if (!json) {
+//         return NextResponse.json({
+//           status: 400,
+//           error: "Code is required",
+//         });
+//       }
 
-      const formatter = new JSONFormatter(json, 2);
-      const formattedJSON = formatter.render().innerHTML;
+//       const formatter = new JSONFormatter(json, 2);
+//       const formattedJSON = formatter.render().innerHTML;
 
-      return NextResponse.json({
-        status: 200,
-        formattedJSON,
-      });
-    } catch (error) {
-      return NextResponse.json({
-        status: 500,
-        error: "Invalid JSON data",
-      });
-    }
-  } else {
-    return NextResponse.json({
-      status: 405,
-      message: `Method ${req.method} Not Allowed`,
-    });
-  }
-}
+//       return NextResponse.json({
+//         status: 200,
+//         formattedJSON,
+//       });
+//     } catch (error) {
+//       return NextResponse.json({
+//         status: 500,
+//         error: "Invalid JSON data",
+//       });
+//     }
+//   } else {
+//     return NextResponse.json({
+//       status: 405,
+//       message: `Method ${req.method} Not Allowed`,
+//     });
+//   }
+// }
